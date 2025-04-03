6b836548f4a941c740b5602cd3b9dbeeda46ff6b
Alok Singh <alok.singh@dreamweaversindia.com>
2025-04-04 01:11:14 +0530

    migration file updated

--- database/migrations/2025_04_03_143703_create_websites_table.php
+++ database/migrations/2025_04_03_143703_create_websites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('websites', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('domain')->unique();
+            $table->string('domain')->unique();
             $table->timestamps();
         });
     }
